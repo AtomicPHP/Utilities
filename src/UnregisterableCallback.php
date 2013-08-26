6c7240bb6fea8b9d5c4b090e13ab7843a11fc331
Niels Nijens <nijens.niels@gmail.com>
2013-08-26 22:14:03 +0200

    Added UnregisterableCallback

---
+++ src/UnregisterableCallback.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace AtomicPHP\Utilities;
+
+use \InvalidArgumentException;
+
+/**
+ * UnregisterableCallback
+ *
+ * @author  Niels Nijens <nijens.niels@gmail.com>
+ * @package AtomicPHP\Utilities
+ **/
+class UnregisterableCallback {
+
+	/**
+	 * The callable function or class method
+	 *
+	 * @access protected
+	 * @var    callable
+	 **/
+	protected $callback;
+
+	/**
+	 * __construct
+	 *
+	 * Creates a new instance of UnregisterableCallback
+	 *
+	 * @access public
+	 * @param  callable $callback
+	 * @return UnregisterableCallback
+	 **/
+	public function __construct($callback) {
+		if (!is_callable($callback) ) {
+			throw new InvalidArgumentException("Argument is not callable callback");
+		}
+
+		$this->callback = $callback;
+	}
+
+	/**
+	 * call
+	 *
+	 * The call handler method. Use this method to call the callback
+	 *
+	 * @access public
+	 * @return mixed
+	 **/
+	public function call() {
+		if ($this->callback == null) {
+			return;
+		}
+
+		$callback = $this->callback;
+
+		return $callback();
+	}
+
+	/**
+	 * unregister
+	 *
+	 * Unregisters the callback
+	 *
+	 * @access public
+	 * @return void
+	 **/
+	public function unregister() {
+		$this->callback = null;
+	}
+}
