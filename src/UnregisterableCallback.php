ff7f9176b0442c85ae85a8fb203f6466fab2c61f
Niels Nijens <nijens.niels@gmail.com>
2013-08-27 22:19:52 +0200

    Fixed indention

--- src/UnregisterableCallback.php
+++ src/UnregisterableCallback.php
@@ -10,60 +10,64 @@ use \InvalidArgumentException;
  * @author  Niels Nijens <nijens.niels@gmail.com>
  * @package AtomicPHP\Utilities
  **/
-class UnregisterableCallback {
+class UnregisterableCallback
+{
+    /**
+     * The callable function or class method
+     *
+     * @access protected
+     * @var    callable
+     **/
+    protected $callback;
 
-	/**
-	 * The callable function or class method
-	 *
-	 * @access protected
-	 * @var    callable
-	 **/
-	protected $callback;
+    /**
+     * __construct
+     *
+     * Creates a new instance of UnregisterableCallback
+     *
+     * @access public
+     * @param  callable $callback
+     * @return UnregisterableCallback
+     * @throws InvalidArgumentException
+     **/
+    public function __construct($callback)
+    {
+        if (!is_callable($callback) ) {
+            throw new InvalidArgumentException("The argument is not callable callback");
+        }
 
-	/**
-	 * __construct
-	 *
-	 * Creates a new instance of UnregisterableCallback
-	 *
-	 * @access public
-	 * @param  callable $callback
-	 * @return UnregisterableCallback
-	 **/
-	public function __construct($callback) {
-		if (!is_callable($callback) ) {
-			throw new InvalidArgumentException("Argument is not callable callback");
-		}
+        $this->callback = $callback;
+    }
 
-		$this->callback = $callback;
-	}
+    /**
+     * call
+     *
+     * The call handler method. Use this method to call the callback
+     *
+     * @access public
+     * @return mixed
+     **/
+    public function call()
+    {
+        if ($this->callback == null) {
+            return;
+        }
 
-	/**
-	 * call
-	 *
-	 * The call handler method. Use this method to call the callback
-	 *
-	 * @access public
-	 * @return mixed
-	 **/
-	public function call() {
-		if ($this->callback == null) {
-			return;
-		}
+        $callback = $this->callback;
 
-		$callback = $this->callback;
+        return $callback();
+    }
 
-		return $callback();
-	}
-
-	/**
-	 * unregister
-	 *
-	 * Unregisters the callback
-	 *
-	 * @access public
-	 * @return void
-	 **/
-	public function unregister() {
-		$this->callback = null;
-	}
+    /**
+     * unregister
+     *
+     * Unregisters the callback
+     *
+     * @access public
+     * @return void
+     **/
+    public function unregister()
+    {
+        $this->callback = null;
+    }
 }
