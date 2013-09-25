443b449ebba0c90816567c0bac91fa29bb3fad4e
Niels Nijens <nijens.niels@gmail.com>
2013-09-25 23:03:38 +0200

    Improved unit tests bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,11 +3,11 @@
  * Bootstrap file for PHPUnit tests
  *
  * @author Niels Nijens <nijens.niels@gmail.com>
- */
+ **/
 spl_autoload_register(function($className) {
     $vendorNamespace = "AtomicPHP\\Utilities\\";
     if (strpos($className, $vendorNamespace) === 0) {
         $classNameFile = substr($className, strlen($vendorNamespace) ) . ".php";
         include __DIR__ . "/../src/" . $classNameFile;
     }
-});
+}, true);
